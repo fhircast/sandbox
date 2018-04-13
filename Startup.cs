8fbef99ac2ec461901846b072a15e40abe8bf753
Leo Bergnéhr <leo.bergnehr@sectra.com>
2018-04-13 15:05:19 +0200

    Add hangfire for backgroud jobs processing

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Hangfire;
 
 namespace FHIRcastSandbox {
     public class Startup {
@@ -13,7 +14,9 @@ namespace FHIRcastSandbox {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-            services.AddMvc(setup => {
+            services.AddMvc();
+            services.AddHangfire(config => {
+                config.UseNLogLogProvider();
             });
         }
 

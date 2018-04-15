c89a7ccdaf9cc3b3d5c9875ad0d620cfaf3ba3e1
Niklas Svenzen <ni-sve@sectra.se>
2018-04-15 10:03:34 +0200

    Added fhircast as the default log file for the log controller

--- Controllers/LogController.cs
+++ Controllers/LogController.cs
@@ -11,7 +11,21 @@ namespace FHIRcastSandbox.Controllers {
     public class LogController : Controller {
         [HttpGet]
         [Route("{log}")]
-        public IActionResult Get(string log = "fhircast") {
+        public IActionResult Get(string log) {
+            var target = LogManager.Configuration.FindTargetByName<FileTarget>(log);
+            if (target != null) {
+                var logFile = target.FileName.Render(new LogEventInfo());
+                var logDir = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().Location).AbsolutePath);
+
+                return this.Content(System.IO.File.ReadAllText(Path.Combine(logDir, logFile)));
+            } else {
+                return this.Content("No such log file found...");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Get() {
+            var log = "fhircast";
             var target = LogManager.Configuration.FindTargetByName<FileTarget>(log);
             var logFile = target.FileName.Render(new LogEventInfo());
             var logDir = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().Location).AbsolutePath);

dfaecc02e3ab3a0c4c937ca4592bb845d3f123aa
Niklas Svenzen <ni-sve@sectra.se>
2018-04-17 02:16:05 +0200

    Added client model
    The model is to be passed between client and server and also between controllers

---
+++ Model/ClientModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json;
+
+namespace FHIRcastSandbox.Model {
+    public class ClientModel {
+        
+        public string UserIdentifier { get; set; }
+        public string PatientIdentifier { get; set; }
+        public string PatientIdIssuer { get; set; }
+        public string AccessionNumber { get; set; }
+        public string AccessionNumberGroup { get; set; }
+        public string StudyId { get; set; }
+
+    }
+
+}
